6d0045707505b5d4f773976fc8cf0b0153a90b5a
zaidsaid-007 <zaidsaid007@gmail.com>
2024-10-04 23:59:06 +0300

    assets

---
+++ src/assets/assets.js
@@ -0,0 +1,41 @@
+import apples from './apples.jpg';
+import bananas from './bananas.jpg';
+import grapes from './grapes.jpg';
+import lime from './lime.jpg';
+import oranges from './oranges.jpg';
+import beef from './beef.jpg';
+import chicken from './chicken.jpg';
+import fish from './fish.jpg';
+import lamb from './lamb.jpg';
+import eggs from './eggs.jpg';
+import milk from './milk.jpg';
+import tomatoes from './tomatoes.jpg';
+import onions from './onions.jpg';
+import cabbages from './cabbages.jpg';
+import fruits_category from './fruits_category.jpg';
+import vegetables_category from './vegetables_category.jpg';
+import meat_category from './meat_category.jpg';
+import juice_category from './juice_category.jpg';
+import logo from './logo.png';
+
+export const assets = {
+    logo,
+    apples,
+    bananas,
+    grapes,
+    lime,
+    oranges,
+    beef,
+    chicken,
+    fish,
+    lamb,
+    eggs,
+    milk,
+    tomatoes,
+    onions,
+    cabbages,
+    fruits_category,
+    vegetables_category,
+    meat_category,
+    juice_category
+}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import { assets } from '../../assets/assets'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <img src={assets.logo} alt='' className='logo'/>
+        
+
+    </div>
+  )
+}
+
+export default Navbar
