ef24928eec38e83da707dfa63fd0115a351125cb
zaidsaid-007 <zaidsaid007@gmail.com>
2024-10-23 02:39:15 +0300

    order Schema

---
+++ backend/Models/orderModel.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const OrderSchema = new mongoose.Schema({
+    userId:{type:String,required:true},
+    items:{type:Array,required:true},
+    amount:{type:Number,required:true},
+    address:{type:Object,required:true},
+    status:{type:String,default:'Pending'},
+    createdAt:{type:Date,default:Date.now()},
+    payment:{type:Boolean,default:false}
+})
+
+const orderModel = mongoose.models.order || mongoose.model('order', OrderSchema);
+
+export default orderModel;
